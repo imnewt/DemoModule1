87bd91707f0af260c5662e65ae8c1de369561221
Kibuto <nguyentrongnhanc8@gmail.com>
2020-02-27 15:30:58 +0700

    change background

--- components/BackgroundImage.js
+++ components/BackgroundImage.js
@@ -3,7 +3,7 @@ import { ImageBackground } from "react-native";
 import bg from "../images/gradient-bg.png"
 
 export default function BackgroundImage(props) {
-    return <ImageBackground source={bg} style={{width: '100%', height: '100%'}}>
+    return <ImageBackground source={{uri: 'https://phonewallpaper.net/wp-content/uploads/2018/11/Gradient-Background-Wallpaper-469x832.jpg'}} style={{width: '100%', height: '100%'}}>
         {props.children}
     </ImageBackground>
 }
