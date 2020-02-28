66b97b1e62b86579d3761a5c024227897caec5c9
imnewt <maitantruc1999@gmail.com>
2020-02-28 13:32:45 +0700

    new header image

--- components/HeaderImage.js
+++ components/HeaderImage.js
@@ -1,13 +1,23 @@
 import React from "react"
-import { Image } from "react-native"
+import { Image, View, StyleSheet, ImageBackground } from "react-native"
 import HI from "../images/performance-management.png"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import { TouchableOpacity } from "react-native-gesture-handler"
+import { useNavigation } from "@react-navigation/native"
+
+
 
 export default function HeaderImage() {
+    const navigation = useNavigation();
     return(
-        <Image 
-        resizeMode='stretch' 
-        style={{width: "100%", height: 150}} 
-        source={HI} 
-        />
+        <ImageBackground 
+            resizeMode='stretch' 
+            style={{width: "100%", height: 150}} 
+            source={HI} 
+        >
+            <TouchableOpacity style={{marginTop: 12, marginLeft: 18}} onPress={() => navigation.openDrawer()}>
+                <Ionicons name="ios-menu" size={40} color="#000"/>
+            </TouchableOpacity>
+        </ImageBackground>
     )
 }
